d415939b2e3e79f3a129c05d92826a2f270e1a0b
Katerina Smirnova <147266542+KaterinaSmi@users.noreply.github.com>
2024-06-03 16:55:28 +0300

    Add files via upload

---
+++  Part1.1_anecdotes/my-react-app/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const BiggestVoice = () => {
+
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleAnecdotes = ()=>{
+      setSelected(selected+1)
+    }
+
+  const voteFor = () =>{
+    const newVotes = [...votes];
+    newVotes[selected]+= 1 
+    setVotes(newVotes);
+  }
+
+  const maxVotesIndex = votes.indexOf(Math.max(...votes));
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick = {voteFor}>vote</button>
+      <button onClick = {handleAnecdotes}>next anecdote</button>
+      
+      <h2>Anecdotes with the most votes</h2>
+      <p>{anecdotes[maxVotesIndex]}</p>
+      <p>has {votes[maxVotesIndex]} votes</p>
+    </div>
+  )
+}
+
+export default App
