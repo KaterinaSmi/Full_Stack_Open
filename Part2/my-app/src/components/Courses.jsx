9a6d8891746adb339de4393d1217c88cef66ff9b
Ekaterina Smirnova <ekaterina.mavritckaia@gmail.com>
2024-07-29 18:00:11 +0300

    Add Part2 with my-app React project

---
+++ Part2/my-app/src/components/Courses.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Courses = ({ courses }) => {
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+  const Part = ({ name, exercises }) => {
+    return (
+      <p>
+        {name} {exercises}
+      </p>
+    );
+  };
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+export default Courses;
